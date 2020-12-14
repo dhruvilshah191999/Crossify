a3118ee254d93efdd7d4d1015cdfe0f63288909f
dhruvilshah191999 <59329841+dhruvilshah191999@users.noreply.github.com>
2020-12-14 19:12:44 +0530

    Update app.js

--- crossify/src/app.js
+++ crossify/src/app.js
@@ -20,23 +20,35 @@ import Landing from "views/Landing.js";
 import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 import SearchPage from "views/SearchPage";
+
+const Routing = () => {
+  const history = useHistory();
+  useEffect(() => {
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      history.push("/auth");
+    }
+  }, []);
+  return (
+    <Switch>
+      {/* add routes with layouts */}
+      <Route path="/admin" component={Admin} />
+      <Route path="/auth" component={Auth} />
+      {/* add routes without layouts */}
+      <Route path="/landing" exact component={Landing} />
+      <Route path="/profile" exact component={Profile} />
+      <Route path="/" exact component={Index} />
+      <Route path="/search" exact component={SearchPage} />
+      {/* add redirect for first page */}
+      <Redirect from="*" to="/" />
+    </Switch>
+  );
+};
+
 export default function App() {
-  let history = useHistory();
-  var token = localStorage.getItem("token");
   return (
     <BrowserRouter>
-      <Switch>
-        {/* add routes with layouts */}
-        <Route path="/admin" component={Admin} />
-        <Route path="/auth" component={Auth} />
-        {/* add routes without layouts */}
-        <Route path="/landing" exact component={Landing} />
-        <Route path="/profile" exact component={Profile} />
-        <Route path="/" exact component={Index} />
-        <Route path="/search" exact component={SearchPage} />
-        {/* add redirect for first page */}
-        <Redirect from="*" to="/" />
-      </Switch>
+      <Routing />
     </BrowserRouter>
   );
 }
