04c57a9951aec88984e86047acf45f090d2011b5
dhruvilshah191999 <59329841+dhruvilshah191999@users.noreply.github.com>
2021-03-08 12:57:10 +0530

    Check

--- crossify/src/components/Maps/MapBoxApi.js
+++ crossify/src/components/Maps/MapBoxApi.js
@@ -1,23 +1,18 @@
 import React,{useState} from "react";
-import mapboxgl from "mapbox-gl";
 import ReactMapGL from "react-map-gl";
 export default function ShowMap(){
     const {viewport,setViewport}=useState({
         latitude:22.732564,
         longitude:78.004253,
-        
         zoom: 10,
         width:"100vw",
         height:"100vh"
     });
-  
     return (
-    
     <div >
-        
     <link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
-    <ReactMapGL 
-        {...viewport} 
+    <ReactMapGL
+        {...viewport}
         mapboxApiAccessToken="pk.eyJ1IjoiYmhhcmdhdi0yNyIsImEiOiJja2xrcHEyZGEwazV6MndxZmF1MHEzb3BhIn0.Z4RIYOak6wvUhhBhos-Iew" 
         onViewportChange={ (viewport)=> {setViewport(viewport)}
         
